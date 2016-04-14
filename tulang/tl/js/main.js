91558af48530ef1a4d43a76150275eb7b27e1ae1
Tony <lostyu789@163.com>
2016-04-14 18:42:07 +0800

    用户中心

--- tulang/tl/js/main.js
+++ tulang/tl/js/main.js
@@ -129,6 +129,13 @@ $(function () {
         }
 
 
+
+        // icheck
+        //$('input').iCheck({
+        //    checkboxClass: 'icheckbox_minimal-blue',
+        //    radioClass: 'iradio_minimal-blue',
+        //    increaseArea: '20%' // optional
+        //});
     })();
 
     // 登录
